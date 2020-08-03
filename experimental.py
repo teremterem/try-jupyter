fef32da1b9f9f42c7ac3122e2deeab2612a830d0
Oleksandr Tereshchenko <toporok@gmail.com>
2020-08-04 01:12:39 +0300

    pandas_map notebook

--- experimental.py
+++ experimental.py
@@ -1,2 +1,7 @@
-if __name__ == '__main__':
-    print('hello world!')
+import pandas as pd
+
+some_frame = pd.DataFrame(data={
+    'col1': ['c11', 'c12', 'c13', 'c14'],
+    'col2': ['c21', 'c22', 'c23', 'c24'],
+    'col3': ['c31', 'c32', 'c33', 'c34'],
+})
