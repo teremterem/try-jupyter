8d1c00d12a50ea1157b4056edcc6b402e211d97a
Oleksandr Tereshchenko <toporok@gmail.com>
2020-08-04 03:22:33 +0300

    dict to dataframe

--- experimental.py
+++ experimental.py
@@ -5,3 +5,10 @@ some_frame = pd.DataFrame(data={
     'col2': ['c21', 'c22', 'c23', 'c24'],
     'col3': ['c31', 'c32', 'c33', 'c34'],
 })
+
+some_dict = {
+    'key1': 'value1',
+    'key2': 'value2',
+    'key3': 'value3',
+}
+some_other_frame = pd.DataFrame(some_dict.items(), columns=['key col', 'value col'])
